05320ac48b9c58788629623c7de39108a8dbb368
adis-tek <67308354+adis-tek@users.noreply.github.com>
2021-03-10 14:27:23 -0600

    styled about section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+//Global Style
+import GlobalStyle from './components/GlobalStyle';
 import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
-      <h1>Film</h1>
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
