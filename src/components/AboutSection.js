2fa2c5282ab1de2bceeb22f65bcedff1f9738860
adis-tek <67308354+adis-tek@users.noreply.github.com>
2021-03-10 12:40:38 -0600

    created about us page

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import home1 from '../img/home1.png';
 
 const AboutSection = () => {
     return (
@@ -18,6 +19,11 @@ const AboutSection = () => {
                         <h2>true.</h2>
                     </div>
                 </div>
+                <p>Contact us for any photography or videography ideas you have. We have proffessionals with amazing skills.</p>
+                <button>Contact Us</button>
+            </div>
+            <div className="image">
+                <img src={home1} alt="guy with a camera"/>
             </div>
         </div>
     );
